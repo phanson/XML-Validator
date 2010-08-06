ff25c94b5dbf313dd00b36467ac66bfb8ff0ad26
phanson <grayman3@gmail.com>
2010-08-05 22:47:54 -0400

    Initial commit

---
+++ XmlValidator/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace XmlValidator
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // aggregator for file validity
+            bool allValid = true;
+
+            // need at least schema and xml file
+            if (args.Length < 2)
+            {
+                Console.WriteLine("usage: XmlValidator.exe <schema> <file1> [<file2> ...]");
+                return 1;
+            }
+
+            // check existence of schema file separately
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("error: file '{0}' does not exist", args[0]);
+                return 1;
+            }
+
+            // spacing purely for aesthetics :)
+            Console.WriteLine("");
+
+            // get arg list and expand wildcards
+            List<string> fileList = new List<string>();
+            fileList.AddRange(args.Skip(1));
+            doExpansion(fileList);
+
+            // process every unique filename
+            foreach (string file in fileList.Distinct().ToList())
+            {
+                if (!File.Exists(file))
+                {
+                    // not throwing an error, because we might have other files to process
+                    Console.WriteLine("\r\n{0}: file does not exist!\r\n", file);
+                    continue;
+                }
+
+                // attempt validation
+                XmlValidator validator = new XmlValidator();
+                if (validator.Validate(args[0], file))
+                {
+                    Console.WriteLine("{0}: validation succeeded", file);
+                }
+                else
+                {
+                    Console.WriteLine("\r\n{0}: validation failed {1}\r\n", file, validator.ErrorMessage);
+                    allValid = false;
+                }
+            }
+
+            return allValid ? 0 : 1;
+        }
+
+        /// <summary>
+        /// Expand all wildcard strings into file names
+        /// </summary>
+        private static void doExpansion(List<string> filenames)
+        {
+            List<string> expandedNames = new List<string>();
+            foreach (string wildcardString in filenames.Where<string>(name => name.Contains("*")))
+                expandedNames.AddRange(Directory.EnumerateFiles(".", wildcardString, SearchOption.TopDirectoryOnly));
+
+            filenames.RemoveAll(name => name.Contains("*"));
+            filenames.AddRange(expandedNames);
+        }
+    }
+}
---
+++ XmlValidator/XmlValidator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XmlValidator
+{
+    /// <summary>
+    /// Handles setting up the XML libraries and validating documents
+    /// </summary>
+    public class XmlValidator
+    {
+        private int errorCount = 0;
+        private string errorMessage = "";
+
+        /// <summary>
+        /// Gets validation error message, if any
+        /// </summary>
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+        }
+
+        private void ValidationHandler(object sender, ValidationEventArgs args)
+        {
+            errorMessage += "\r\n" + args.Message;
+            errorCount++;
+        }
+
+        /// <summary>
+        /// Validates an XML file using the given schema.
+        /// </summary>
+        /// <param name="schemaURI">URI of schema</param>
+        /// <param name="xmlURI">URI of document</param>
+        /// <returns><c>true</c> if validation is successful</returns>
+        public bool Validate(string schemaURI, string xmlURI)
+        {
+            XmlReaderSettings s = new XmlReaderSettings();
+            s.ValidationFlags = XmlSchemaValidationFlags.ReportValidationWarnings | XmlSchemaValidationFlags.ProcessIdentityConstraints;
+            s.ValidationType = ValidationType.Schema;
+            s.ValidationEventHandler += new ValidationEventHandler(ValidationHandler);
+            s.Schemas.Add(null, schemaURI);
+
+            XmlReader reader = XmlReader.Create(xmlURI, s);
+
+            while (reader.Read()) ;
+
+            reader.Close();
+            return errorCount == 0;
+        }
+    }
+}
